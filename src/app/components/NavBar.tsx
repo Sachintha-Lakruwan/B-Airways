bb4707a576072c3f47210f36dedebb97520d0854
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-08 18:53:45 +0530

    Add Home and NavBar components

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className=" w-full h-16 bg-zinc-700 relative">
+      <div className=" absolute left-20 top-0 h-40 aspect-[5/8] bg-blue-600 rounded-b-md drop-shadow-xl"></div>
+    </div>
+  );
+}
