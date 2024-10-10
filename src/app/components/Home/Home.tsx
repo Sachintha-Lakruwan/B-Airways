e3f13e3debd24c2cbbcc0a1c160dfa260839a6eb
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-10 17:45:57 +0530

    Refactor Home component and update Hero image gradient

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
           alt="hero image"
         ></Image>
       </div>
-      <div className=" w-full absolute bottom-0 h-1/2 bg-gradient-to-t from-zinc-300 to-transparent"></div>
+      <div className=" w-full absolute bottom-0 h-3/5 bg-gradient-to-t from-zinc-300 opacity-80 to-transparent"></div>
       <div className=" glass-text pl-20 flex items-center absolute left-0 w-1/2 aspect-[4/1] top-[18%] to-transparent">
         <p className=" text-7xl font-bold italic text-white">
           Beyond your expectations
