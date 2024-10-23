f05bd31d97b388efc5995e462b7b944f44c60c8a
L0rd008 <152273571+L0rd008@users.noreply.github.com>
2024-10-23 19:16:28 +0530

    added backticks as a fix to the query

--- src/app/api/flightsearch/classes/route.ts
+++ src/app/api/flightsearch/classes/route.ts
@@ -4,8 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const seatClasses = await executeQuery(
-      "SELECT 'id' as 'key', 'name' as 'label' FROM seat_class;"
+      "SELECT `id` as `key`, `name` as `label` FROM `seat_class`;"
     );
+    console.log(seatClasses)
     return NextResponse.json(seatClasses);
   } catch (error) {
     console.error("Error fetching seat classes:", error);
