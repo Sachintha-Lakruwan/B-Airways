585a4d1259100c067f59d6d9f488ed59f959a308
L0rd008 <152273571+L0rd008@users.noreply.github.com>
2024-10-23 18:59:25 +0530

    added queries for dynamic routing - classes

--- src/app/api/flightsearch/classes/route.ts
+++ src/app/api/flightsearch/classes/route.ts
@@ -1,11 +1,14 @@
+import { executeQuery } from "../../database/database";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const classes = [
-    { key: "F", label: "First Class" },
-    { key: "J", label: "Business Class" },
-    { key: "W", label: "Premium Economy" },
-    { key: "Y", label: "Economy Class" },
-  ];
-  return NextResponse.json(classes);
+  try {
+    const seatClasses = await executeQuery(
+      "SELECT 'id' as 'key', 'name' as 'label' FROM seat_class;"
+    );
+    return NextResponse.json(seatClasses);
+  } catch (error) {
+    console.error("Error fetching seat classes:", error);
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+  }
 }
