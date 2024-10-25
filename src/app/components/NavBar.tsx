701eadfddf424d5a384884863e50f881d5b0ac49
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-25 22:21:24 +0530

    Refactor NavBar component to handle logout functionality

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,7 +4,11 @@ import React, { useEffect } from "react";
 import logo from "@/public/B Airways.png";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
-import { checkTokenValidity, logout, setToken } from "../GlobalRedux/Slices/auth/authSlice";
+import {
+  checkTokenValidity,
+  logout,
+  setToken,
+} from "../GlobalRedux/Slices/auth/authSlice";
 import { RootState } from "../GlobalRedux/store";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -13,26 +17,24 @@ export default function NavBar() {
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const handleClick = ()=>{
-    dispatch(logout())
+  const handleClick = () => {
+    dispatch(logout());
     router.push("/");
-  }
+  };
 
   useEffect(() => {
-      const token = localStorage.getItem("token");
-      if (token) {
-          dispatch(setToken(token)); // Set token in the Redux store
-          dispatch(checkTokenValidity()); // Check if the token is still valid
-      }
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(setToken(token)); // Set token in the Redux store
+      dispatch(checkTokenValidity()); // Check if the token is still valid
+    }
   }, [dispatch]);
 
-  const authState = useSelector(
-    (state: RootState) => state.auth
-  );
+  const authState = useSelector((state: RootState) => state.auth);
   console.log(authState);
 
   return (
-    <div className=" w-full h-[10%] bg-zinc-700 absolute z-10 nav-glass px-20">
+    <div className=" w-full h-[10%] bg-zinc-700 absolute z-30 nav-glass px-20">
       {/* <div className=" absolute left-20 top-0 h-32 aspect-[3/4] bg-sky-600 rounded-b-md drop-shadow-xl">
         <div className=" m-2 absolute w-full aspect-square bg-slate-500 bottom-0"></div>
       </div> */}
@@ -40,8 +42,7 @@ export default function NavBar() {
         <div className=" flex flex-row gap-6">
           <div className=" h-20 aspect-square p-2 relative">
             <Link href="/">
-              <Image src={logo} alt="B Airways Logo" fill sizes="100vw">
-              </Image>
+              <Image src={logo} alt="B Airways Logo" fill sizes="100vw"></Image>
             </Link>
           </div>
           <div className=" flex items-center">
@@ -60,23 +61,19 @@ export default function NavBar() {
             </Link>
           </div>
         </div>
-        {!authState.isAuthenticated ? 
-        (  
-        <Link href="/login">
-          <div>
-            <div>LOGIN</div>
-          </div>
-        </Link>
-        ) 
-        : 
-        (
-        <button onClick={handleClick}>
-          <div>
-            <div>LOGOUT</div>
-          </div>
-        </button>
-        )
-        }
+        {!authState.isAuthenticated ? (
+          <Link href="/login">
+            <div>
+              <div>LOGIN</div>
+            </div>
+          </Link>
+        ) : (
+          <button onClick={handleClick}>
+            <div>
+              <div>LOGOUT</div>
+            </div>
+          </button>
+        )}
       </div>
     </div>
   );
