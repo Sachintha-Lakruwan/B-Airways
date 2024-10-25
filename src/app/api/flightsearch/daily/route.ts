e4c68b2e4dfd583f94b4285651b74e5536967e02
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-10-25 22:57:54 +0530

    Added api endpoint to show daily flights

---
+++ src/app/api/flightsearch/daily/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { executeQuery } from "../../database/database";
+
+interface Schedule {
+    key: number;
+    date: string;
+    departure_time: string;
+    duration: string;
+    departure_airport: string;
+    arrival_airport: string;
+    model: string;
+    departure_country: string;
+    arrival_country: string;
+}
+
+export async function GET() {
+    const countries = await executeQuery(
+        "SELECT schedule.`id` AS `key`, schedule.`date`, route.departure_time, route.duration, route.departure AS `departure_airport`, route.arrival AS `arrival_airport`, airplane_model.name AS `model`, SUBSTRING_INDEX(departure_location.Full_Hierarchy, '>', 1) AS 'departure_country', SUBSTRING_INDEX(arrival_location.Full_Hierarchy, '>', 1) AS 'arrival_country' FROM schedule LEFT JOIN route ON schedule.flight_code=route.flight_code LEFT JOIN locationhierarchy AS arrival_location ON route.arrival = arrival_location.code LEFT JOIN locationhierarchy AS departure_location ON route.departure = departure_location.code LEFT JOIN airplane ON schedule.airplane_number=airplane.tail_number LEFT JOIN airplane_model ON airplane.model_id=airplane_model.id WHERE schedule.date=?;",
+        [new Date("2024-10-15").toISOString().slice(0, 10)]
+    ) as Schedule[];
+    return NextResponse.json(countries);
+}
