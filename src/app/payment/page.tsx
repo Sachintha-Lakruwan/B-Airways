6dd07f1b9f8339ab2fac3ea1aca08032ddaa043d
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-10-28 20:09:27 +0530

    Refactor package.json and package-lock.json to update framer-motion dependency version

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,11 +2,15 @@
 
 import { Button, Input } from "@nextui-org/react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import img from "@/public/payment.jpg";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "../GlobalRedux/store";
+import check from "@/public/check.png";
+import { motion } from "framer-motion";
+import { FaClipboard } from "react-icons/fa";
+import { FaClipboardCheck } from "react-icons/fa";
 
 interface Details {
   flight: number;
@@ -28,6 +32,12 @@ export default function Page() {
   const searchParams = useSearchParams();
   const flightId = Number(searchParams.get("flight"));
   const seatNumber = searchParams.get("seat");
+  const [error, setError] = useState<boolean>(false);
+  const [isSuccessful, setSuccessful] = useState<boolean>(false);
+  const [reference, setReference] = useState<string>("H458S23J56G");
+  const [clipBoardEnable, setClipBoardEnable] = useState<boolean>(true);
+
+  const router = useRouter();
 
   const handleSubmit = async () => {
     details.flight = flightId;
@@ -46,7 +56,15 @@ export default function Page() {
       },
       body: JSON.stringify(details),
     });
-    console.log(response);
+    if (response.status !== 200) {
+      setError(true);
+      console.log("error");
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
+    } else {
+      setSuccessful(true);
+    }
   };
 
   function handleConfirmPayment() {
@@ -82,13 +100,62 @@ export default function Page() {
           >
             Pay Now
           </Button>
-          {/* {pickSeatWarning && (
+          {error && (
             <div className=" absolute mt-1 text-center text-red-800 italic text-sm bottom-0">
-              Please pick a Seat
+              Something went wrong! Please try again later..
             </div>
-          )} */}
+          )}
         </div>
       </div>
+      {isSuccessful && (
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ opacity: 100, scale: 1 }}
+          className=" absolute w-full h-full glass2 flex items-center flex-col justify-around py-[8%]"
+        >
+          <h1 className=" text-5xl font-extrabold text-sky-100">
+            Payement Successful!{" "}
+          </h1>
+          <div className=" relative w-[20%] aspect-square">
+            <Image
+              src={check}
+              fill
+              sizes="100vw"
+              alt="transaction successful"
+              objectFit="cover"
+            ></Image>
+          </div>
+          <div className=" text-sky-50 font-bold itali glass3 rounded-full flex gap-2 items-center">
+            <p className=" py-2 pl-4">Your reference number is</p>{" "}
+            <p>{reference}</p>
+            <span className=" h-full aspect-square rounded-full bg-white flex items-center justify-center drop-shadow-2xl">
+              {clipBoardEnable ? (
+                <FaClipboard
+                  size="1.3em"
+                  className=" cursor-pointer text-sky-900"
+                  onClick={() => {
+                    setClipBoardEnable(false);
+                    navigator.clipboard.writeText(reference);
+                    setTimeout(() => {
+                      setClipBoardEnable(true);
+                    }, 2500);
+                  }}
+                />
+              ) : (
+                <FaClipboardCheck size="1.2em" className=" text-green-700" />
+              )}
+            </span>
+          </div>
+          <Button
+            className="h-14 w-48 bg-sky-100 drop-shadow-2xl opacity-75 rounded-full"
+            variant="solid"
+            color="primary"
+            onClick={() => router.push("/")}
+          >
+            <p className="font-bold text-lg text-sky-900">Home</p>
+          </Button>
+        </motion.div>
+      )}
     </div>
   );
 }
