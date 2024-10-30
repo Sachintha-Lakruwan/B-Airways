6358a7dd805f0d213c89919b86d2609c3713ba2a
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-10-30 10:24:35 +0530

    Changed autofill so that it returns country_code

--- src/app/api/booking/autofill/route.tsx
+++ src/app/api/booking/autofill/route.tsx
@@ -20,7 +20,7 @@ export async function GET(request : NextRequest) {
     try {
         if (token !== null) {
             decode = jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret") as { userId : number, role : string ,iat : number, exp : number };
-            const res = await executeQuery("SELECT passenger.name, passenger.age, passenger.gender, passenger.NIC, passenger.passport_number, country.name AS `country` FROM `registered_user` LEFT JOIN `passenger` ON registered_user.passenger_id = passenger.id LEFT JOIN country ON country.code = passenger.country_code WHERE registered_user.id = ?", [decode.userId]) as AutofillDetails[];
+            const res = await executeQuery("SELECT passenger.name, passenger.age, passenger.gender, passenger.NIC, passenger.passport_number, passenger.country_code FROM `registered_user` LEFT JOIN `passenger` ON registered_user.passenger_id = passenger.id WHERE registered_user.id = ?", [decode.userId]) as AutofillDetails[];
             // console.log(res);
             return NextResponse.json(res[0]);
         }
